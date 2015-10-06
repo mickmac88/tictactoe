87c17f0cd4c9e3dfd2bb9d4e33a91aa98892685e
Mick S <mickmac88@gmail.com>
2015-10-06 14:32:41 -0400

    re-added users page

--- routes/users.js
+++ routes/users.js
@@ -3,20 +3,26 @@ var router = express.Router();
 
 var User = require('../models').User;
 
-// router.session('user_id', function(req, res, next) {
-//   User.findById(req.session.user_id).then(function(u) {
-//     req.user = res.locals.user = u;
-//     next();
-//   });
-// });
+router.param('user_id', function(req, res, next) {
+  User.findById(req.params.user_id).then(function(u) {
+    req.user = res.locals.user = u;
+    next();
+  });
+});
+
+router.get('/', function(req, res) {
+    User.findAll().then(function(users) {
+        res.render('users', { users: users });
+    });
+});
 
 router.get('/usernameExists', function(req, res) {
   User.doesUsernameExist(req.query.username)
     .then(res.json.bind(res));
 });
 
-// router.get('/:user_id', function(req, res) {
-//   res.render('individualUser');
-// });
+router.get('/:user_id', function(req, res) {
+  res.render('individualUser');
+});
 
 module.exports = router;

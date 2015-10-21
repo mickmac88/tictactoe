91e6f7248a60d941e7c23a1f336809e551981b90
Mick S <mickmac88@gmail.com>
2015-10-21 10:20:51 -0400

    post install; add redis; Postgresql setup; lowercasing

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 'use strict';
 module.exports = function(sequelize, DataTypes) {
-  var User = sequelize.define('User', {
+  var User = sequelize.define('user', {
     first_name: DataTypes.STRING,
     last_name: DataTypes.STRING,
     username: DataTypes.STRING,
